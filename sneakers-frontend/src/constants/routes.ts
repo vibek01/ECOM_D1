08fda6d8cec4b6f27544770a49da482b88b3a138
vibek01 <vibekbin305@gmail.com>
2025-09-16 22:42:20 +0530

    Updaing the constants folder to match with the new types

---
+++ sneakers-frontend/src/constants/routes.ts
@@ -0,0 +1,14 @@
+export const ROUTES = {
+  HOME: '/',
+  PRODUCTS: '/products',
+  PRODUCT_DETAIL: '/product/:id', // The ':id' is a placeholder for the product's ID
+  NEW_RELEASES: '/new-releases',
+  SALE: '/sale',
+  CART: '/cart',
+  CHECKOUT: '/checkout',
+  LOGIN: '/login',
+  REGISTER: '/register',
+  PROFILE: '/profile',
+  SEARCH: '/search',
+  NOT_FOUND: '*', // Represents any unmatched route
+};
